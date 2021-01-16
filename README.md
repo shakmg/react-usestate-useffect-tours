0fbf0c2f65aed8d89f612fdf65aca73d90f46b9f
Shakeel Mohammed <shakeel.mohammed@thetrainline.com>
2021-01-16 15:43:42 +0000

    create a sample app to display tours using the useState and useEffect hooks

---
+++ README.md
@@ -0,0 +1,5 @@
+## Description
+
+A create-react-app example course exercise using the useState and useEffect hooks to display travel tours.
+
+Allows tours to be removed, refreshed and some toggle functionality around how much tour description text to display.
---
+++ src/Tours.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours, removeTour}) => {
+  return <section>
+    <div className="title">
+      our tours
+      <div className="underline"></div>
+    </div>
+    <div>
+      {tours.map(tour => {
+        return <Tour key={tour.id} {...tour} removeTour={removeTour}/>
+      })}
+    </div>
+  </section>
+};
+
+export default Tours;
